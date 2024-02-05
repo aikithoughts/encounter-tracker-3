1f4e0ade8656d9e818ea69c3a2959d91ed7e0e60
Dave Shevitz <shevitz@stripe.com>
2024-02-05 14:29:38 -0800

    initial commit

---
+++ src/components/EncounterHeader.jsx
@@ -0,0 +1,9 @@
+const EncounterHeader = () => {
+    return (
+        <div className="header-container">
+          <h2>Encounter Tracker</h2>
+        </div>
+    )
+}
+
+export default EncounterHeader;
